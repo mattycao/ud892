ab35c788afa1cc482b9a7c17c7d8cc1daf3aaa5d
mattycao <mattycao@gmail.com>
2016-01-24 17:13:55 +0800

    the first lesson about eslint and optimization

--- lesson3/gulpfile.js
+++ lesson3/gulpfile.js
@@ -3,18 +3,18 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['styles'], function() {
-	gulp.watch('sass/**/*.scss', ['styles']);
-	browserSync.init({
-		server: './'
-	});
+gulp.task('default', ['styles'], function () {
+    gulp.watch('sass/**/*.scss', ['styles']);
+    browserSync.init({
+        server: './'
+    });
 });
 
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css')).pipe(browserSync.stream());
+gulp.task('styles', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./css')).pipe(browserSync.stream());
 });
